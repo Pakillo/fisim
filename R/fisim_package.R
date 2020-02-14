dccf5ced46027939c03039a93badf84bde508927
Robert Nuske <robert.nuske@nw-fva.de>
2020-02-14 16:42:42 +0100

    make global variables known to check

--- R/fisim_package.R
+++ R/fisim_package.R
@@ -7,3 +7,9 @@
 #' @docType package
 #' @import data.table spatstat sp rgeos
 NULL
+
+# avoid R CMD check warnings due to global variables
+utils::globalVariables(c("N", "d", "dbh", "ef", "ef_alt1", "ef_alt2", "f_edge",
+                         "id_point", "id_sample", "id_stem", "id_tree",
+                         "is_edge", "n", "n_corr", "s", "variable", "x_s",
+                         "x_tree", "x_wt", "y_s", "y_tree", "y_wt"))
